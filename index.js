c1da4d3804d09f31a2e56b29f9b24b544a3c8eef
jasenkaz <jasenkaz@yahoo.com>
2017-06-14 20:07:23 -0400

    stubbed backbone of app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express'); //grabbing everything from the folder
+const server = express(); //it calls the server
+
+const port = 8080;
+//__dirname is node only, its a short cut for looking for files in the working directory
+server.use(express.static(__dirname + '/public'));
+
+server.get('/Recounter', (request, response) => {
+  response.sendFile('public/html/index.html', {root: __dirname});
+}); //telling the server what to listen for; fat arrow is the same as writing function(request, response){}
+server.listen(port, () => {
+  console.log('Now listening on port ', port);
+});

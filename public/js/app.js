69b68da6f5965a848202ffc3c5ed4a0bb24e952c
jasenkaz <jasenkaz@yahoo.com>
2017-06-15 20:56:50 -0400

    added the ability to change color on click if the app selected color is different

--- public/js/app.js
+++ public/js/app.js
@@ -25,16 +25,25 @@ cacheDOM: function(){
   this.root = document.querySelector(this.rootElement);
 },
 bindEvents: function(){},
-render: function(){
-  this.grid.forEach(row => {
+changeColor: function(rowIndex, colIndex){
+  this.grid[rowIndex][colIndex];
+  const cell = this.grid[rowIndex][colIndex];
+  cell.color = this.selectedColor;
+  this.render();
+},
+render: function(){               //mixing up html with java makes it quicker and easier to change properties like height and width of the cell above; if it was hardcoded in js only, you cant change it easy
+  this.root.innerHTML = '';
+  this.grid.forEach((row, rowIndex) => {
     const rowContainer = document.createElement('div');
     rowContainer.style.height = `${this.cellHeight}px`;
-    row.forEach(cell => {
+    row.forEach((cell, colIndex) => {
       const element = cell.toHtml();
+      element.addEventListener('click', () => this.changeColor(rowIndex, colIndex));
       rowContainer.appendChild(element);     //rows are now containers
     });
     this.root.appendChild(rowContainer);
   });
+
 },
 }
 App.start();

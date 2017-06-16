58ebd553129a8cdd562dd6ada1e6428e07bfbad5
jasenkaz <jasenkaz@yahoo.com>
2017-06-15 20:33:12 -0400

    refactor the make grid method. Implemented the render method to draw the grid on the page that required the cell to produce a DOM element

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 const App = {
   rootElement: '#app',
   selectedColor: 'white',
-  numRows: 4,
-  numCols: 4,
-  cellWidth: 100,
-  cellHeight: 100,
+  numRows: 10,
+  numCols: 10,
+  cellWidth: 25,
+  cellHeight: 25,
   grid: [],                 //grid is an array of array
   start: function(){
     this.cacheDOM();
@@ -14,17 +14,27 @@ const App = {
   },
 makeGrid: function(){
   this.grid = new Array(this.numRows);
-  for(let i = 0; i < this.grid.length; i += 1){
-    this.grid[i] = new Array(this.numCols);
-  }
-  for(let i = 0; i < this.grid.length; i += 1){
-    for(let j = 0; j < this.grid.lenght; j += 1){
-    this.grid[i] [j] = new Cell(this.cellWidth, this.cellHeight, this.cellColor);
+  for(let rowIndex = 0; rowIndex < this.numRows; rowIndex += 1){        //starts the row at index 0, less than how many you have, increment by 1, reiterates it by adding 1
+  this.grid[rowIndex] = new Array(this.numCols);
+  for(let colIndex = 0; colIndex < this.numCols; colIndex += 1){
+    this.grid[rowIndex] [colIndex] = new Cell(this.cellWidth, this.cellHeight, this.cellColor);
     }
   }
 },
-cacheDOM: function(){},
+cacheDOM: function(){
+  this.root = document.querySelector(this.rootElement);
+},
 bindEvents: function(){},
-render: function(){},
+render: function(){
+  this.grid.forEach(row => {
+    const rowContainer = document.createElement('div');
+    rowContainer.style.height = `${this.cellHeight}px`;
+    row.forEach(cell => {
+      const element = cell.toHtml();
+      rowContainer.appendChild(element);     //rows are now containers
+    });
+    this.root.appendChild(rowContainer);
+  });
+},
 }
 App.start();
--- public/js/cell.js
+++ public/js/cell.js
@@ -17,5 +17,16 @@
 function Cell(width, height, color){
   this.width = width || 100; //browser choses width you type inside () of new Cell(), or defaults to 100; so if you type new Cell(200), it will give Cell{200,100,'red'}
   this.height = height || 100;
-  this.color = color || 'red';
+  this.color = color || 'white';
 }                           //this kind of behaves like c++, its up to the developer, will do similar as above factory function; it gives us info in the browser upon inspection
+
+//add a method to the data above
+Cell.prototype.toHtml = function(){
+  const div = document.createElement('div');     //height and width need a unit, ie pixels, so put it in the ``
+  div.style.height = `${this.height}px`;
+  div.style.display = 'inline-block';
+  div.style.width = `${this.width}px`;
+  div.style.border = '0.25px solid black';
+  div.style.backgroundColor = this.color;
+  return div;
+};             //cell prototype is the template that the cell object is built on top of

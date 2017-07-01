0af598318e5bfe79daca36e6f9e6fae5e23b8819
jasenkaz <jasenkaz@yahoo.com>
2017-07-01 19:16:01 -0400

    prepped for hiroku upload and fixed the grid. Also added in some css

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express'); //grabbing everything from the folder
 const server = express(); //it calls the server
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 //__dirname is node only, its a short cut for looking for files in the working directory
 server.use(express.static(__dirname + '/public'));
 
 server.get('/', (request, response) => {
   response.sendFile('public/html/index.html', {root: __dirname});
 }); //telling the server what to listen for; fat arrow is the same as writing function(request, response){}
-server.listen(port, () => {
+server.listen(8080, () => {
   console.log('Now listening on port ', port);
 });
--- public/js/cell.js
+++ public/js/cell.js
@@ -26,7 +26,7 @@ Cell.prototype.toHtml = function(){
   div.style.height = `${this.height}px`;
   div.style.display = 'inline-block';
   div.style.width = `${this.width}px`;
-  div.style.border = '0.25px solid black';
+  div.style.border = '0.5px solid gray';
   div.style.backgroundColor = this.color;
   return div;
 };             //cell prototype is the template that the cell object is built on top of
